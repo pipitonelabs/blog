4031cced2543df20a009f515ca37bdd52353fe70
Joe Pipitone <joseph.pipitone@gmail.com>
2025-11-24 12:38:44 -0500

    feat: blog test

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: 'astro-erudite',
+  title: 'josephpipitone',
   description:
     'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  href: 'https://astro-erudite.vercel.app',
-  author: 'jktrn',
+  href: 'https://blog.tslamars.com',
+  author: 'josephpipitone',
   locale: 'en-US',
   featuredPostCount: 2,
   postsPerPage: 3,
@@ -28,7 +28,7 @@ export const NAV_LINKS: SocialLink[] = [
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/jktrn',
+    href: 'https://github.com/josephpipitone',
     label: 'GitHub',
   },
   {
@@ -36,7 +36,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
     label: 'Twitter',
   },
   {
-    href: 'mailto:jason@enscribe.dev',
+    href: 'mailto:joseph.pipitone@gmail.com',
     label: 'Email',
   },
   {
